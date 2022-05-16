8bafcfad9d975688063787eb1063ec86e5f00899
Justin Lepine <lepine.justin@yahoo.com>
2022-05-16 18:41:34 -0400

    created custom blender3d object converted and imported

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import './App.scss';
 import Box from './components/Box/Box';
 import Sphere from './components/Sphere/AnimatedSphere';
+import Hero from './components/Hero';
 import { OrbitControls } from '@react-three/drei';
 import {Canvas} from '@react-three/fiber';
 
@@ -10,17 +11,25 @@ function App() {
     <div className="app">
       <Canvas className='app__canvas'>
         <OrbitControls enableZoom={false} />
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <ambientLight intensity={ 0.5 } />
+        <directionalLight position={ [ -2, 5, 2 ] } intensity={ 1 } />
         <Suspense fallback={null}>
+        <Hero />
+        </Suspense>
+      </Canvas>
+      <Canvas className='app__canvas'>
+        <OrbitControls enableZoom={ false } />
+        <ambientLight intensity={ 0.5 } />
+        <directionalLight position={ [ -2, 5, 2 ] } intensity={ 1 } />
+        <Suspense fallback={ null }>
         <Box />
         </Suspense>
       </Canvas>
       <Canvas className='app__canvas'>
-        <OrbitControls enableZoom={false} enableRotate={false} />
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[-2, 5, 2]} intensity={1} />
-        <Suspense fallback={null}>
+        <OrbitControls enableZoom={false} enableRotate={ false } />
+        <ambientLight intensity={ 0.5 } />
+        <directionalLight position={ [ -2, 5, 2 ] } intensity={ 1 } />
+        <Suspense fallback={ null }>
         <Sphere />
         </Suspense>
       </Canvas>

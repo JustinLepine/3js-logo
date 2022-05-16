890bfa2f5ef00c39f6469d418fb878a8785b5d60
Justin Lepine <lepine.justin@yahoo.com>
2022-05-16 10:11:40 -0400

    concrete square created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
+import React, { Suspense } from 'react';
+import './App.scss';
 import Box from './components/Box';
-
+import { OrbitControls } from '@react-three/drei';
 import {Canvas} from '@react-three/fiber';
 
 function App() {
   return (
-    <div className="App">
-      <Canvas>
-        <Box/>
+    <div className="app">
+      <Canvas className='app__canvas'>
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+        <Box />
+        </Suspense>
       </Canvas>
     </div>
   );
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,5 +1,15 @@
 import React from "react";
+import { useLoader } from '@react-three/fiber';
+import { TextureLoader } from "three";
+import texture from '../assets/textures/flat-grey.jpg'
 
 export default function Box() {
-    return <div/>
+    const colorMap = useLoader(TextureLoader, texture);
+    
+    return (
+        <mesh rotation={[90,0,20]}>
+            <boxBufferGeometry attach='geometry' args={[3,3,3]}/>
+            <meshStandardMaterial map={colorMap} />
+        </mesh>
+    )
 }
